e7e450c0d75059048d6293105a7f87801b676dd2
Jan Eirik Hallstensen <janh@mesan.no>
2012-10-12 15:35:31 +0200

    angular.js endringer

--- TimeRegistrering/public/javascripts/controllers.js
+++ TimeRegistrering/public/javascripts/controllers.js
@@ -1,43 +1,38 @@
-function ListTimeRegCtrl($scope) {
-	$scope.registrerteTimer = [
-			{"id" : "1",
-			"ansattnr" : "123",
-			"dato" : "asd",
-			"arbeid" : "arbeid 1",
-			"timer" : "7,5",
-			"overtid" : "false"},
-			{"id" : "2",
-				"ansattnr" : "123",
-				"dato" : "asd",
-				"arbeid" : "arbeid 2",
-				"timer" : "3,5",
-				"overtid" : "true"},
-			{"id" : "3",
-				"ansattnr" : "123",
-				"dato" : "asd",
-				"arbeid" : "arbeid 3",
-				"timer" : "7,5",
-				"overtid" : "false"}
-	];
-		
-	$scope.test = "Hello world!";
-	$scope.test2 = "asdasd";
-};
+var timereg = angular.module('timereg', []);
 
+timereg.controller('TimeregCtrl', function TimeregCtrl($scope, timeregStorage) {
+var registrerteTimer = $scope.registrerteTimer = timeregStorage.get();
+	$scope.leggTilTimereg = function(user) {
+		timeregStorage.put(user);
+	};
+});
 
-function RegistrerTimerCtrl($scope) {
-	$scope.ansattnr = "";
-	$scope.dato = "";
-	$scope.arbeid = "";
-	$scope.timer = "";
-	$scope.overtid = "";
-	$scope.kommentar = "";
-	
-	$scope.setAnsattnr = function(new_ansattnr) {
-		this.ansattnr = new_ansattnr;
-    }
-	
-	$scope.printstatus = function() {
-		alert("Ansatt: " + this.ansattnr + " Dato: " + this.dato + " Arbeid: " + this.arbeid + " Timer: " + this.timer + " Overtid: " + this.overtid + " Kommentar: " + this.kommentar);
-	}
-};
+timereg.factory('timeregStorage', function() {
+	return {
+		get: function() {
+			return [
+           			{"id" : "1",
+           			"ansattnr" : "123",
+           			"dato" : "asd",
+           			"arbeid" : "arbeid 1",
+           			"timer" : "7,5",
+           			"overtid" : "false"},
+           			{"id" : "2",
+           				"ansattnr" : "123",
+           				"dato" : "asd",
+           				"arbeid" : "arbeid 2",
+           				"timer" : "3,5",
+           				"overtid" : "true"},
+           			{"id" : "3",
+           				"ansattnr" : "123",
+           				"dato" : "asd",
+           				"arbeid" : "arbeid 3",
+           				"timer" : "7,5",
+           				"overtid" : "false"}
+           	];
+		},
+		put: function(user) {
+			alert("save" + user.ansattnr);
+		}
+	};
+});
