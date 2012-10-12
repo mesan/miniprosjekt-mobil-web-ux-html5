cafb3ffbd89b6e72aeedcd7529931ea7f2f27082
Ayhan <ail@forskningsradet.no>
2012-10-12 15:19:30 +0200

    Html siden inkluderer ingen css fil, ressursene blir lastet inn utifra behovet for hver enkelt device type. Bruker categorizr.js til å finne ut om devicen er av typen desktop eller mobil. Bruker yepnope.js til å laste inn tilhørende CSS

--- TimeRegistrering/public/javascripts/categorizrplayground/a-cat.js
+++ TimeRegistrering/public/javascripts/categorizrplayground/a-cat.js
@@ -1,3 +1,27 @@
 aMain = function() {
   console.log("aMain executed, current device type: " +  categorizr());
+
+  jQuery(window).bind('deviceChange', function(device) {
+    console.log(device.toString() + " ++ Log from aMain");
+  });
+
+  //categorizr('desktop', 'mobile');
+
+  console.log(categorizr());
+
+  var erMobil = function() {
+    console.log("ouehufehu");
+    if(categorizr() == 'mobile'){
+      return true;
+    }else {
+      return false;
+    }
+  };
+
+  yepnope({
+      test: erMobil() ,
+      yep: '/assets/stylesheets/responsive_mobile.css',
+      nope: '/assets/stylesheets/responsive_desktop.css'
+  });
+
 };
